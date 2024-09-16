bcc1d7daf69c95cb665fb83f82989fac809c3215
hunterellenberger <hlelle01@louisville.edu>
2024-09-16 17:07:23 -0400

    added a dictionary containing the information of touching nodes

---
+++ tspBFS&DFS.py
@@ -0,0 +1,16 @@
+from tspbruteforce import get_coordinates, calc_distance
+
+touchingNodes = {
+    1: [2, 3, 4],
+    2: [3],
+    3: [4, 5],
+    4: [5, 6, 7],
+    5: [7, 8],
+    6: [8],
+    7: [9, 10],
+    8: [9, 10, 11],
+    9: [11],
+    10: [11],
+    11: []
+}
+
