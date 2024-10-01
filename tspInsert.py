3fc342a52df7fca662357d790325ffbbe7c88f62
hunterellenberger <hlelle01@louisville.edu>
2024-09-30 20:34:31 -0400

    implemented greedy_insert and find_shortest_path methods

--- tspInsert.py
+++ tspInsert.py
@@ -1,21 +1,44 @@
 from tspUtil import get_coordinates, calc_distance, plotter
-from collections import deque
+from sys import maxsize
 
 tspFile = open("Random30.tsp", "r")
 coordinates = {}
 x = []
 y = []
 
+def find_shortest_path(node, listOfPaths, nodeAndCoordinates):
+    shortestDistance = [None, maxsize]
+    tempDistance = 0
 
-def greedy_insert(start, pathPlusDistance):
+    for element in listOfPaths:
+        tempDistance = calc_distance(nodeAndCoordinates[node][0], nodeAndCoordinates[node][1], nodeAndCoordinates[element][0], nodeAndCoordinates[element][1])
+        if tempDistance != 0 and tempDistance < shortestDistance[1]:
+            shortestDistance = [element, tempDistance]
+    print(shortestDistance)
+    print()
+    return shortestDistance
+
+def greedy_insert(start, nodePlusCoordinate):
     path = []
-    for i in range(start, len(pathPlusDistance)):
-        for j in range():
-            print("placeholder")
+    notTouchedNodes = []
+
+    for element in nodePlusCoordinate:
+        notTouchedNodes.append(element)
+
+    while notTouchedNodes:
+        path.append(start)
+        greedyPath = find_shortest_path(start, notTouchedNodes, nodePlusCoordinate)
+        notTouchedNodes.remove(start)
+        start = greedyPath[0]
+    
+    return path
+
+
+    print(notTouchedNodes)
 
 
 get_coordinates(tspFile, coordinates)
-greedy_insert(0, coordinates)
+print(greedy_insert(1, coordinates))
 plotter(coordinates)
 
 
