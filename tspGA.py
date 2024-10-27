f05e80375f338e16a20788a7f18997530b19ae61
hunterellenberger <hlelle01@louisville.edu>
2024-10-27 18:19:20 -0400

    made and fixed weed generation

--- tspGA.py
+++ tspGA.py
@@ -1,14 +1,21 @@
 from tspUtil import get_coordinates, calc_distance
 from random import shuffle
-from copy import deepcopy
 
 #initialization of variables to be used throughout program
 tspFile = open("Random100.tsp", "r")
-baseList = []       #holds original 1-100 nodes
-populationOne = []  #holds one population that gets mixed with population 2
-populationTwo = []  #holds one population that gets mixed with population 1
 coordinates = {}    #holds nodes and their coordinates
 
+#calculates distance of a path
+def path_distance(path, nodeAndDistance):
+    totalLength = 0
+    for i in range(0, len(path)):
+        print(i)
+        if i == len(path) - 1:
+            totalLength += calc_distance(nodeAndDistance[path[i]][0], nodeAndDistance[path[i]][1], nodeAndDistance[path[-1]][0], nodeAndDistance[path[-1]][1])
+            return totalLength
+        else:
+            totalLength += calc_distance(nodeAndDistance[path[i]][0], nodeAndDistance[path[i]][1], nodeAndDistance[path[i + 1]][0], nodeAndDistance[path[i + 1]][1])
+    return totalLength
 
 #makes original generation of paths
 def make_paths(numberOfPaths, numberOfNodes):
@@ -19,12 +26,23 @@ def make_paths(numberOfPaths, numberOfNodes):
         initialPaths.append(randomPath)
     return initialPaths
 
-
+#weeds out old generation
+def weed(baseGeneration, nodeAndDistance):
+    survivingGeneration = []
+    shuffle(baseGeneration)
+    midpoint = len(baseGeneration) // 2
+    for i in range(midpoint):
+        if path_distance(baseGeneration[i], nodeAndDistance) < path_distance(baseGeneration[i + midpoint], nodeAndDistance):
+            survivingGeneration.append(baseGeneration[i])
+        else:
+            survivingGeneration.append(baseGeneration[i + midpoint])
+    return survivingGeneration
 
 
 get_coordinates(tspFile, coordinates)
-print(make_paths(20, 100))
-
+x = make_paths(60, 100)
+y = weed(x, coordinates)
+print(y)
 
 
 
